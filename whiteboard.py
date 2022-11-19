dd7ad835d83157fdef535d7b2e41391e26b82864
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-11-19 17:45:00 +0800

    Added: Informations and tkinter

---
+++ whiteboard.py
@@ -0,0 +1,9 @@
+# Name: Jerico James F. Viteño
+# Laboratory Exercise 3: Whiteboard
+# November 19, 2022
+
+from tkinter import *
+from tkinter.colorchooser import askcolor
+from tkinter import ttk
+import tkinter as tk
+
