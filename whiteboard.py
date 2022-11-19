60e40824f334a60b7765639b18b9fd88d3d623cc
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-11-19 19:11:39 +0800

    Added: Color Palette

--- whiteboard.py
+++ whiteboard.py
@@ -12,11 +12,48 @@ root.title("White Board")
 root.geometry("1050x570+130+50")
 root.resizable(False,False)
 
+colors = Canvas(root, bg='#ffffff', width=37, height=300, bd=0)
+colors.place(x=30, y=60)
+
+def displayPalette():
+    id = colors.create_rectangle((10,10,30,30),fill='black')
+    colors.tag_bind(id, 'Button-1',lambda x: show_color('black'))
+
+    id = colors.create_rectangle((10,40,30,60),fill='gray')
+    colors.tag_bind(id, 'Button-1',lambda x: show_color('gray'))
+   
+    id = colors.create_rectangle((10,70,30,90),fill='brown4')
+    colors.tag_bind(id, 'Button-1',lambda x: show_color('brown4'))
+   
+    id = colors.create_rectangle((10,100,30,120),fill='red')
+    colors.tag_bind(id, 'Button-1',lambda x: show_color('red'))
+    
+    id = colors.create_rectangle((10,130,30,150),fill='orange')
+    colors.tag_bind(id, 'Button-1',lambda x: show_color('orange'))
+   
+    id = colors.create_rectangle((10,160,30,180),fill='yellow')
+    colors.tag_bind(id, 'Button-1',lambda x: show_color('yellow'))
+    
+    id = colors.create_rectangle((10,190,30,210),fill='green')
+    colors.tag_bind(id, 'Button-1',lambda x: show_color('green'))
+    
+    id = colors.create_rectangle((10,220,30,240),fill='blue')
+    colors.tag_bind(id, 'Button-1',lambda x: show_color('blue'))
+    
+    id = colors.create_rectangle((10,250,30,270),fill='purple')
+    colors.tag_bind(id, 'Button-1',lambda x: show_color('purple'))
+
+    
+
+displayPalette()
+
 canvas = Canvas(root,width=930,height=500,background="white",cursor="hand2")
 canvas.place(x=100,y=10)
 
-canvas.bind('<Button-1')
-canvas.bind('B1-Motion')
+canvas.bind('<Button-1', locate_xy)
+canvas.bind('B1-Motion', addLine)
+
+
 
 
 root.mainloop()
