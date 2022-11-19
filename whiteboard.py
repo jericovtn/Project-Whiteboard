625f0c9de4bdc676322e3916cffd3e3091e18a6c
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-11-19 18:00:35 +0800

    Added: Roots

--- whiteboard.py
+++ whiteboard.py
@@ -7,3 +7,11 @@ from tkinter.colorchooser import askcolor
 from tkinter import ttk
 import tkinter as tk
 
+root = Tk()
+root.title("White Board")
+root.geometry("1050x570+150+50")
+root.configure(bg="f2f3f5")
+root.resizable(False,False)
+
+#icon
+
