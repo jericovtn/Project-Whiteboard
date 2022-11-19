9008823c3d10beab34565b084130563483166ba0
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-11-19 20:33:17 +0800

    Finalization

--- whiteboard.py
+++ whiteboard.py
@@ -13,32 +13,30 @@ root.geometry("1050x570+150+50")
 root.configure(bg='#f2f3f5')
 root.resizable(False,False)
 
-# icons
-
-current_x = 0
-current_y = 0
+currentX = 0
+currentY = 0
 color = 'black'
 
-def locate_xy(work):
-    global current_x, current_y 
-    current_x = work.x
-    current_y = work.y
+def locateXY(work):
+    global currentX, currentY 
+    currentX = work.x
+    currentY = work.y
 
 def addLine(work):
-    global current_x, current_y 
-    canvas.create_line((current_x, current_y, work.x, work.y), 
-    width=get_current_value(), fill=color, capstyle=ROUND, smooth=TRUE)
-    current_x, current_y = work.x, work.y
+    global currentX, currentY 
+    canvas.create_line((currentX, currentY, work.x, work.y), 
+    width=getCurrentValue(), fill=color, capstyle=ROUND, smooth=TRUE)
+    currentX, currentY = work.x, work.y
 
-def show_color(new_color):
+def showColor(new_color):
     global color
     color = new_color
 
-def new_canvas():
+def newCanvas():
     canvas.delete('all')
     displayPalette()
 
-btn = Button(root, text = 'Clear', bd='5', command=new_canvas).place(x=30, y=400)
+button = Button(root, text = 'Clear', bd='5', command=newCanvas).place(x=30, y=400)
 
 #Colors
 colors = Canvas(root, bg='#ffffff', width=37, height=300, bd=0)
@@ -46,31 +44,31 @@ colors.place(x=30, y=60)
 
 def displayPalette():
     id = colors.create_rectangle((10,10,30,30),fill='black')
-    colors.tag_bind(id, '<Button-1>',lambda x: show_color('black'))
+    colors.tag_bind(id, '<Button-1>',lambda x: showColor('black'))
 
     id = colors.create_rectangle((10,40,30,60),fill='gray')
-    colors.tag_bind(id, '<Button-1>',lambda x: show_color('gray'))
+    colors.tag_bind(id, '<Button-1>',lambda x: showColor('gray'))
    
     id = colors.create_rectangle((10,70,30,90),fill='brown4')
-    colors.tag_bind(id, '<Button-1>',lambda x: show_color('brown4'))
+    colors.tag_bind(id, '<Button-1>',lambda x: showColor('brown4'))
    
     id = colors.create_rectangle((10,100,30,120),fill='red')
-    colors.tag_bind(id, '<Button-1>',lambda x: show_color('red'))
+    colors.tag_bind(id, '<Button-1>',lambda x: showColor('red'))
     
     id = colors.create_rectangle((10,130,30,150),fill='orange')
-    colors.tag_bind(id, '<Button-1>',lambda x: show_color('orange'))
+    colors.tag_bind(id, '<Button-1>',lambda x: showColor('orange'))
    
     id = colors.create_rectangle((10,160,30,180),fill='yellow')
-    colors.tag_bind(id, '<Button-1>',lambda x: show_color('yellow'))
+    colors.tag_bind(id, '<Button-1>',lambda x: showColor('yellow'))
     
     id = colors.create_rectangle((10,190,30,210),fill='green')
-    colors.tag_bind(id, '<Button-1>',lambda x: show_color('green'))
+    colors.tag_bind(id, '<Button-1>',lambda x: showColor('green'))
     
     id = colors.create_rectangle((10,220,30,240),fill='blue')
-    colors.tag_bind(id, '<Button-1>',lambda x: show_color('blue'))
+    colors.tag_bind(id, '<Button-1>',lambda x: showColor('blue'))
     
     id = colors.create_rectangle((10,250,30,270),fill='purple')
-    colors.tag_bind(id, '<Button-1>',lambda x: show_color('purple'))
+    colors.tag_bind(id, '<Button-1>',lambda x: showColor('purple'))
 
     
 displayPalette()
@@ -78,24 +76,24 @@ displayPalette()
 canvas = Canvas(root, width=930, height=500, background="white", cursor="hand2")
 canvas.place(x=100,y=10)
 
-canvas.bind('<Button-1>', locate_xy)
+canvas.bind('<Button-1>', locateXY)
 canvas.bind('<B1-Motion>', addLine)
 
-current_value = tk.DoubleVar()
+currentValue = tk.DoubleVar()
 
-def get_current_value():
-    return '{: .2f}'.format(current_value.get())
+def getCurrentValue():
+    return '{: .2f}'.format(currentValue.get())
 
 def slider_changed(event):
-    value_label.configure(text=get_current_value())
+    valueLabel.configure(text=getCurrentValue())
 
 # Slider
 slider = ttk.Scale(root, from_=0, to=100, orient='horizontal', command=slider_changed, 
-variable=current_value)
+variable=currentValue)
 slider.place(x=30, y=530)
 
 # Value Label
-value_label = ttk.Label(root,text=get_current_value())
-value_label.place(x=27, y=550)
+valueLabel = ttk.Label(root,text=getCurrentValue())
+valueLabel.place(x=27, y=550)
 
 root.mainloop()
